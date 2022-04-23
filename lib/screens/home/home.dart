5ff53929300c11e545b71d5c95f8691541a6d87d
SAIF H K M <hishamkolikara@gmail.com>
2022-04-23 13:20:40 +0530

    Added: Enable Location Dialog

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,8 +1,15 @@
 import 'package:adhanminima/screens/home/components/homeWidget.dart';
 import 'package:adhanminima/screens/home/components/panelWidget.dart';
+import 'package:adhanminima/utils/sizedbox.dart';
+import 'package:adhanminima/utils/theme.dart';
+import 'package:android_intent/android_intent.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
+import 'package:restart_app/restart_app.dart';
+import 'package:lottie/lottie.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -14,6 +21,13 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   final panelController = PanelController();
 
+  void openLocationSetting() async {
+    const AndroidIntent intent = AndroidIntent(
+      action: 'android.settings.LOCATION_SOURCE_SETTINGS',
+    );
+    await intent.launch();
+  }
+
   Future<Position> _determinePosition() async {
     bool serviceEnabled;
     LocationPermission permission;
@@ -21,9 +35,64 @@ class _HomeState extends State<Home> {
     // Test if location services are enabled.
     serviceEnabled = await Geolocator.isLocationServiceEnabled();
     if (!serviceEnabled) {
-      // Location services are not enabled don't continue
-      // accessing the position and request users of the
-      // App to enable the location services.
+      //print("Location disabled");
+      showDialog(
+          context: context,
+          builder: (context) {
+            var dialogContext = context;
+            return AlertDialog(
+              title: const Center(
+                  child: Text('Enable Location',
+                      style: TextStyle(
+                          fontFamily: 'Halenoir',
+                          color: Colors.grey,
+                          fontSize: 24,
+                          fontWeight: FontWeight.w600))),
+              actions: <Widget>[
+                Center(
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(20, 0, 20, 8),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Lottie.asset('assets/location-animation.json'),
+                        const Text(
+                            "We need to know your location in order to calculate the prayer timings.",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                fontFamily: 'Halenoir',
+                                color: Colors.black87,
+                                fontSize: 14,
+                                fontWeight: FontWeight.w500)),
+                        verticalBox(15),
+                        Container(
+                          height: 40,
+                          width: double.infinity,
+                          child: TextButton(
+                              style: TextButton.styleFrom(
+                                  primary: Colors.white,
+                                  backgroundColor: Colors.blue),
+                              onPressed: () async {
+                                openLocationSetting();
+                                Navigator.pop(dialogContext);
+                              },
+                              child: const Text('Turn on location service')),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            SystemChannels.platform
+                                .invokeMethod('SystemNavigator.pop');
+                          },
+                          child: const Text('Not Now'),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            );
+          });
+      //openLocationSetting();
       return Future.error('Location services are disabled.');
     }
 
